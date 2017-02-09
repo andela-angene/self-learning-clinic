be2bfc0b7bd5b326121ae819dbf56a26c31c4fa7
Anthony Ngene <ngeneanthony@gmail.com>
2017-02-09 09:45:27 +0100

    created the function to solve the fizzbuzz problem

---
+++ Fizz-Buzz/fizz-buzz.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = function (n) {
+  if (n % 5 == 0 && n % 3 == 0){
+    return 'FizzBuzz';
+  }else if(n % 3 == 0){
+    return 'Fizz';
+  }else if(n % 5 == 0){
+    return 'Buzz';
+  }else{
+    return n;
+  }
+}
